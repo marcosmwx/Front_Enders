91f2029bac1b04dff1a106e2326eeb3524e06a79
Marcos Willian <87791042+marcosmwx@users.noreply.github.com>
2022-10-26 16:56:39 -0300

    Atualização Index
    Incremento de estilos e links em toda pagina

--- script_index/script.js
+++ script_index/script.js
@@ -45,3 +45,39 @@ const nav = document.querySelector("nav"),
              
             }, 0)
         }());
+        const debounce = function(func, wait, immediate) {
+          let timeout;
+          return function(...args) {
+            const context = this;
+            const later = function () {
+              timeout = null;
+              if (!immediate) func.apply(context, args);
+            };
+            const callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+          };
+        };
+        
+const target = document.querySelectorAll('[data-anime');
+const animationClass = 'animate';
+
+function animeScroll(){
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3 )/4);
+  target.forEach(function(el){
+
+      if (windowTop > el.offsetTop){
+        el.classList.add(animationClass);
+    } else{
+      el.classList.remove(animationClass)
+
+    }
+
+    
+ })
+
+}
+window.addEventListener('scroll', function(){
+  animeScroll();
+});
